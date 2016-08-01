6e74f2430019e26e64919d80d6b0f21eb811ac9f
Andy Orahoske <andrew.orahoske@zipscene.com>
2016-08-01 10:45:04 -0400

    ember-cli-lodash-extras initial commit
    ember-browserify
    
    lodash-extras initializer

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
-/* jshint node: true */
-'use strict';
+/* eslint-env node, amd */
+/* eslint no-var: "off" */
+/* eslint prefer-template: "off" */
+/* eslint object-shorthand: "off" */
+
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
+var path = require('path');
 
 module.exports = {
-  name: 'ember-cli-lodash-extras'
+  name: 'ember-cli-lodash-extras',
+
+  included: function(target) {
+    var app, lodashExtrasPath;
+
+    this._super.included.apply(this, arguments);
+
+    app = target.app || target;
+    lodashExtrasPath = 'vendor/lodash-extras';
+
+    app.import(path.join(lodashExtrasPath, 'lodash-extras.min.js'));
+
+  },
+
+  treeForVendor: function(vendorTree) {
+    var trees = [];
+    var lodashExtras = path.dirname(require.resolve('lodash-extras/dist/client/lodash-extras-w-ember.min'));
+
+    if (vendorTree) trees.push(vendorTree);
+
+    trees.push(new Funnel(lodashExtras, { destDir: 'lodash-extras' }));
+
+    return mergeTrees(trees);
+
+  }
+
+
 };
